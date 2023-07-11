70f10076822190a855df712b715eb1ac61d53ddd
Jared <jchud13@gmail.com>
2023-07-11 18:34:07 -0400

    Init commit to create repo

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+type authResponse struct {
+    Key     string  `json:"secretKey"`
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/secretKey", getSecretKeyByFile)
+
+    router.Run("localhost:8080")
+}
+
+func getSecretKeyByFile(c *gin.Context) {
+	fmt.Println("here")
+	retKey := &authResponse{}     
+	fileBytes, _ := os.ReadFile("WeatherKey.json")   
+	err := json.Unmarshal(fileBytes, retKey)
+	fmt.Println(retKey)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+    c.IndentedJSON(http.StatusOK, retKey)
+}
